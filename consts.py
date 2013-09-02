f1fad8c1c0b463655c51f14545a9c02ca35ed633
henri <mots@motscousus.com>
2013-09-02 19:14:51 +0200

    add scale IonEol

--- consts.py
+++ consts.py
@@ -48,7 +48,8 @@ MUSICAL_MODES = [
  'Kumoi',			[0,2,3,7,9],
  'Pelog',			[0,1,3,4,7,8],
 
- 'Spanish',			[0,1,3,4,5,6,8,10]
+ 'Spanish',			[0,1,3,4,5,6,8,10],
+ 'IonEol',			[0,2,3,4,5,7,8,9,10,11] #from aschneiderg@unal.edu.co
 ]
 
 def index_of(list, elt):
