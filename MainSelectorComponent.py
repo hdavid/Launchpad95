ee19b854e38303ab04f38a15165962d94ddde161
Poltow <poltow@gmail.com>
2016-08-19 11:04:05 -0300

    On screen MODE message added

--- MainSelectorComponent.py
+++ MainSelectorComponent.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import Live
 from DeviceComponent import DeviceComponent
 from InstrumentControllerComponent import InstrumentControllerComponent
 import Settings
@@ -13,8 +12,6 @@ from _Framework.ButtonMatrixElement import ButtonMatrixElement
 from _Framework.ModeSelectorComponent import ModeSelectorComponent
 from _Framework.SessionZoomingComponent import DeprecatedSessionZoomingComponent# noqa
 
-log_enabled = False
-setup_log_enabled = False
 class MainSelectorComponent(ModeSelectorComponent):
 
 	""" Class that reassigns the button on the launchpad to different functions """
@@ -23,8 +20,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 	#	self._control_surface.log_message((' ' + message + ' ').center(50, '='))
 
 	def __init__(self, matrix, top_buttons, side_buttons, config_button, osd, control_surface):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.__init__ start")
 		#verify matrix dimentions
 		assert isinstance(matrix, ButtonMatrixElement)
 		assert ((matrix.width() == 8) and (matrix.height() == 8))
@@ -101,8 +96,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 		self._all_buttons = tuple(self._all_buttons)
 
 	def disconnect(self):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.disconnect start")
 		for button in self._modes_buttons:
 			button.remove_value_listener(self._mode_value)
 
@@ -117,15 +110,11 @@ class MainSelectorComponent(ModeSelectorComponent):
 		self._nav_buttons = None
 		self._config_button = None
 		ModeSelectorComponent.disconnect(self)
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.disconnect end")
 			
 	def session_component(self):
 		return self._session
 
 	def _update_mode(self):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._update_mode start")
 		mode = self._modes_heap[-1][0] #get first value of last _modes_heap tuple. _modes_heap tuple structure is (mode, sender, observer) 
 
 		assert mode in range(self.number_of_modes()) # 8 for this script
@@ -147,16 +136,9 @@ class MainSelectorComponent(ModeSelectorComponent):
 			self._main_mode_index = mode
 			self.update()
 			
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._update_mode end")
-
 	def set_mode(self, mode):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.set_mode start mode=" + str(mode))
 		self._clean_heap()
 		self._modes_heap = [(mode, None, None)]
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.set_mode end")
 		# if ((self.__main_mode_index != mode) or (mode == 3) or True):
 		# 	self._main_mode_index = mode
 		# 	self._update_mode()
@@ -169,8 +151,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 		self.update()
 
 	def _update_mode_buttons(self):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._update_mode_buttons start")
 		self._modes_buttons[0].set_on_off_values("Mode.Session.On","Mode.Session.Off")
 		self._modes_buttons[3].set_on_off_values("Mode.Mixer.On","Mode.Mixer.Off")
 		mode1 = self.getSkinName(Settings.USER_MODES[self._sub_mode_list[1]])
@@ -183,8 +163,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 				self._modes_buttons[index].turn_on()
 			else:
 				self._modes_buttons[index].turn_off()
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._update_mode_buttons end")
 			
 	def getSkinName(self, user2Mode):
 		if user2Mode=="instrument":
@@ -205,9 +183,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 		# in this code, midi channels start at 0.
 		# so channels range from 0 - 15.
 		# mapping to 1-16 in the real world
-
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.channel_for_current_mode start")
 
 		if self._main_mode_index == 0:
 			new_channel = 0  # session
@@ -235,16 +210,11 @@ class MainSelectorComponent(ModeSelectorComponent):
 			# mixer uses base channel 7 and the 4 next ones.
 			new_channel = 6 + self._sub_modes.mode()  # 6,7,8,9,10
 
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.channel_for_current_mode end returning=" + str(new_channel))
-
 		return new_channel
 
 				
 	
 	def update(self):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.update start")
 		assert (self._modes_buttons != None)
 		if self.is_enabled():
 
@@ -270,12 +240,8 @@ class MainSelectorComponent(ModeSelectorComponent):
 			#self.log_message("main selector update")
 			#for line in traceback.format_stack():
 			#	self.log_message(line.strip())
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent.update end")
 		
 	def _setup_usermode(self, mode):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_usermode start mode=" + str(mode))
 		self._disable_all_modes()
 		
 		if mode == "session":
@@ -320,13 +286,7 @@ class MainSelectorComponent(ModeSelectorComponent):
 			self._osd.mode = mode.capitalize()
 			self._osd.update()
 			
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_usermode end")
-		
 	def _setup_session(self, as_active, as_navigation_enabled):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_session start as_active=" + str(as_active) + 
-						"  as_navigation_enabled=" + str(as_navigation_enabled))
 		assert isinstance(as_active, type(False))#assert is boolean
 		for button in self._nav_buttons:
 			if as_navigation_enabled:
@@ -391,12 +351,7 @@ class MainSelectorComponent(ModeSelectorComponent):
 			self._session.set_track_bank_buttons(None, None)
 			self._session.set_scene_bank_buttons(None, None)
 			
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_session end")
-
 	def _setup_instrument_controller(self, as_active):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_instrument_controller start as_active=" + str(as_active))
 		if self._instrument_controller != None:
 			if as_active:
 				self._activate_matrix(False) #Disable matrix buttons (clip slots)
@@ -412,12 +367,8 @@ class MainSelectorComponent(ModeSelectorComponent):
 						button.use_default_message()# Reset to original channel
 						button.force_next_send()#Flush
 			self._instrument_controller.set_enabled(as_active)#Enable/disable instrument controller
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_instrument_controller end")
 			
 	def _setup_device_controller(self, as_active):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_device_controller start as_active=" + str(as_active))		
 		if self._device_controller != None:
 			if as_active:
 				self._activate_scene_buttons(True)
@@ -430,15 +381,8 @@ class MainSelectorComponent(ModeSelectorComponent):
 			else:
 				self._device_controller._is_active = False
 				self._device_controller.set_enabled(False)
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_device_controller end")				
 
 	def _setup_user_mode(self, release_matrix=True, release_side_buttons=True, release_nav_buttons=True, drum_rack_mode=True):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_user_mode start release_matrix=" + str(release_matrix) 
-						+ ", release_side_buttons=" + str(release_side_buttons) 
-						+ ", release_nav_buttons=" + str(release_nav_buttons) 
-						+ ", drum_rack_mode=" + str(drum_rack_mode))	
 		# user1 -> All True but release_nav_buttons / user2 -> All false 
 		for scene_index in range(8):
 			scene_button = self._side_buttons[scene_index]
@@ -461,25 +405,16 @@ class MainSelectorComponent(ModeSelectorComponent):
 		if drum_rack_mode:#User1 enabled
 			self._config_button.send_value(2)#Set LP drum rack layout grid mapping mode
 		self._config_button.send_value(32)#Send enable flashing led config message to LP
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_user_mode end")	
 
 	def _disable_all_modes(self):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._disable_all_modes start")	
 		self._setup_session(False, False)
 		self._setup_instrument_controller(False) 			
 		self._setup_device_controller(False)
 		self._setup_step_sequencers(False, self._stepseq)
 		self._setup_step_sequencers(False, self._stepseq2)
 		self._setup_mixer(False)
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._disable_all_modes end")	
 				
 	def _setup_step_sequencers(self, as_active, sequencer):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_step_sequencers start as_active=" + str(as_active) 
-						+ ", sequencer=" + str(sequencer))		
 		if(sequencer != None):
 			#if(sequencer.is_enabled() != as_active):
 			if as_active:
@@ -490,12 +425,8 @@ class MainSelectorComponent(ModeSelectorComponent):
 				sequencer.set_enabled(True)
 			else:
 				sequencer.set_enabled(False)
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_step_sequencers end")					
 
 	def _setup_mixer(self, as_active):
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_mixer start as_active=" + str(as_active))			
 		assert isinstance(as_active, type(False))
 		if as_active:
 			self._activate_navigation_buttons(True)
@@ -507,12 +438,8 @@ class MainSelectorComponent(ModeSelectorComponent):
 			self._sub_modes.release_controls()
 
 		self._sub_modes.set_enabled(as_active)
-		if(setup_log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._setup_mixer end")	
 
 	def _init_session(self):
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._init_session start")		
 		#self._session.set_stop_clip_value("Session.StopClip")
 		#self._session.set_stop_clip_triggered_value("Session.ClipTriggeredStop")
 		
@@ -538,8 +465,6 @@ class MainSelectorComponent(ModeSelectorComponent):
 		#self._zooming.set_stopped_value("Zooming.Stopped")
 		#self._zooming.set_selected_value("Zooming.Selected")
 		#self._zooming.set_playing_value("Zooming.Playing")
-		if(log_enabled):
-			Live.Base.log("LOG: MainSelectorComponent._init_session end")
 
 	def _activate_navigation_buttons(self, active):
 		for button in self._nav_buttons:
