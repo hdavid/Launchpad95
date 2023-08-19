f78c8b184eb2c71abe100f63592e73e6009df024
henri <mots@motscousus.com>
2023-08-19 13:05:16 +0200

    fix logging

--- Log.py
+++ Log.py
@@ -3,14 +3,14 @@ import os
 USERNAME = os.getlogin()
 from .Settings import Settings
 
-with open(f'C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt', 'a') as f:
-    if Settings.LOGGING:
+if Settings.LOGGING:
+    with open(f'C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt', 'a') as f:
         f.write('====================\n')
 log_num = 0
 
 def log(message):
     global log_num
-    with open(f'C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt', 'a') as f:
-        if Settings.LOGGING:
+    if Settings.LOGGING:
+        with open(f'C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt', 'a') as f:
             f.write(str(log_num) + ' ' + message + '\n')
     log_num += 1
