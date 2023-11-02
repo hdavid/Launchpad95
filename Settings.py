5511772030141f69c6a8765fbbe33d130044c645
henri <mots@motscousus.com>
2023-11-02 06:53:29 +0100

    pep8

--- Settings.py
+++ Settings.py
@@ -1,33 +1,33 @@
 
 class Settings():
-	# Add Stop buttons at the bottom of the Session. Experimental
-	#SESSION__STOP_BUTTONS = True
-	SESSION__STOP_BUTTONS = False
+    # Add Stop buttons at the bottom of the Session. Experimental
+    # SESSION__STOP_BUTTONS = True
+    SESSION__STOP_BUTTONS = False
 
-	# Link sessions between multiple launchpad95. Experimental
-	#SESSION__LINK = True
-	SESSION__LINK = False
+    # Link sessions between multiple launchpad95. Experimental
+    # SESSION__LINK = True
+    SESSION__LINK = False
 
-	# Link stepseq to follow session. Experimental
-	#STEPSEQ__LINK_WITH_SESSION = True
-	STEPSEQ__LINK_WITH_SESSION = False
+    # Link stepseq to follow session. Experimental
+    # STEPSEQ__LINK_WITH_SESSION = True
+    STEPSEQ__LINK_WITH_SESSION = False
 
-	# Should the step sequencer scroll automatically to the currently playing page
-	STEPSEQ__AUTO_SCROLL = False
+    # Should the step sequencer scroll automatically to the currently playing page
+    STEPSEQ__AUTO_SCROLL = False
 
-	# configure what user modes buttons do.
-	# the 3 first value configure the 3 sub modes of button user mode 1, 
-	# and following ones are for user mode 2 button
-	USER_MODES_1 = [
-			"instrument" 
-			,"device" 
-			#,"user 1"
-	]
-	USER_MODES_2 = [
-			 "drum stepseq"
-			 ,"melodic stepseq" 
-			 #,"user 2"
-	]
+    # configure what user modes buttons do.
+    # the 3 first value configure the 3 sub modes of button user mode 1,
+    # and following ones are for user mode 2 button
+    USER_MODES_1 = [
+		"instrument"
+		, "device"
+		# ,"user 1"
+    ]
+    USER_MODES_2 = [
+		"drum stepseq"
+		, "melodic stepseq"
+		# ,"user 2"
+    ]
 
 
 
@@ -41,10 +41,10 @@ class Settings():
 	USE_CUSTOM_DEVICE_CONTROL_COLORS = False
 
 
-	#Logging feature for debugging (creates C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt)
-	LOGGING = False
+    # Logging feature for debugging (creates C:/Users/{USERNAME}/Documents/Ableton/User Library/Remote Scripts/log.txt)
+    LOGGING = False
 
-	# Map buttons to levels in volume slider. Exactly 7 values must be provided.
-	# The lowest button is always set to -inf. Lowest supported value is -69 dB.
-	# So far the values are not exact: -24 dB below equals -23.7 dB in Ableton.
-	VOLUME_LEVELS = (6, 0, -6, -12, -18, -24, -42)
+    # Map buttons to levels in volume slider. Exactly 7 values must be provided.
+    # The lowest button is always set to -inf. Lowest supported value is -69 dB.
+    # So far the values are not exact: -24 dB below equals -23.7 dB in Ableton.
+    VOLUME_LEVELS = (6, 0, -6, -12, -18, -24, -42)
